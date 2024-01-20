bd780cc9c26bc886756ae0af61c12ed6f389ff23
stoicos <marcelo.anavia.web@gmail.com>
2024-01-20 17:01:59 -0300

    Create a prompt to enter the grid size

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
+let body = document.querySelector("body")
+
+let button = document.createElement("button")
+button.textContent = "Create Grid";
+button.addEventListener('click', () => {
+    removeGrid() 
+    size = parseInt(prompt("Ingresa el tamaño"))
+
+    if(size > 100) {
+        return alert("Fuera de las capacidades")
+    }
+
+    generateGrid(size)
+})
+body.append(button)
+
 let container = document.createElement("div")
 container.classList.add("container")
-let body = document.querySelector("body")
 body.append(container)
 
 
-for (let i = 0; i < 16; i++){
-    let row = document.createElement("div");
-    row.classList.add("row")
-    for(let i = 0; i < 16; i++) {
-        let div = document.createElement("div")
-        div.classList.add("cell")
-        div.addEventListener('mouseover', () => {
-            div.classList.add("cell-hovered")
-        })
-        row.append(div)
+function generateGrid(size) {
+    for (let i = 0; i < size; i++){
+        let row = document.createElement("div");
+        row.classList.add("row")
+        for(let i = 0; i < size; i++) {
+            let div = document.createElement("div")
+            div.classList.add("cell")
+            div.addEventListener('mouseover', () => {
+                div.classList.add("cell-hovered")
+            })
+            row.append(div)
+        }
+        container.append(row)
     }
-    container.append(row)
 }
+
+function removeGrid() {
+    container.innerHTML = '';
+}
+
+generateGrid(16)
